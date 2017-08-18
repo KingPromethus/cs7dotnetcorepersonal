179b30b1b5c596ab1cc1fdcfb9303b3601243dda
Marc Charette <mb_charette@hotmail.com>
2017-08-18 08:47:52 -0400

    Creating repo

---
+++ Chapter02/Ch02_Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ch02_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
